fa6b797ef5db27f6da4260f1ffecbdc4ca6a00bf
Bruce Ide <FlyingRhenquest@gmail.com>
2025-12-26 12:04:48 -0700

    Ran code through clang-format

--- src/NodeEditorWindow.cpp
+++ src/NodeEditorWindow.cpp
@@ -18,50 +18,47 @@
 
 namespace fr::Imgui {
 
-  void NodeEditorWindow::Begin() {
-      // I always want my NodeEditorWindow behind all the other windows
-      // and I want it to always fill the entire OS Window that ImGui creates
-      // as the background window.
-      auto flags =
-        ImGuiWindowFlags_NoBringToFrontOnFocus |
-        ImGuiWindowFlags_NoTitleBar |
-        ImGuiWindowFlags_MenuBar |
-        ImGuiWindowFlags_AlwaysVerticalScrollbar | 
-        ImGuiWindowFlags_AlwaysHorizontalScrollbar;
-      ImVec2 displaySize = ImGui::GetMainViewport()->Size;
-      ImGui::SetNextWindowSize(displaySize);
-      ImGui::SetNextWindowPos(ImVec2(0,0));
-      ImGui::Begin(_label.c_str(), nullptr, flags);
-  }
-  
-  void NodeEditorWindow::begin() {
-      Parent::begin();
-      if (ImGui::BeginMainMenuBar()) {
-
-        // File Menu
-        
-        if (ImGui::BeginMenu("File")) {
-          if (ImGui::MenuItem("Exit")) {
-            exitEvent();
-          }
-          
-          ImGui::EndMenu();
-        }
-
-        // Render Registration-based windows
-        for (auto [item, infoVec] : _menus) {
-          if (ImGui::BeginMenu(item.c_str())) {
-            for (auto info : infoVec) {
-              if (ImGui::MenuItem(info->name.c_str())) {
-                info->create();
-              }
-            }
-            ImGui::EndMenu();
-          }
-        }
-        
-      }
-      ImGui::EndMainMenuBar();
-  }
-  
+void NodeEditorWindow::Begin() {
+  // I always want my NodeEditorWindow behind all the other windows
+  // and I want it to always fill the entire OS Window that ImGui creates
+  // as the background window.
+  auto flags = ImGuiWindowFlags_NoBringToFrontOnFocus |
+               ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_MenuBar |
+               ImGuiWindowFlags_AlwaysVerticalScrollbar |
+               ImGuiWindowFlags_AlwaysHorizontalScrollbar;
+  ImVec2 displaySize = ImGui::GetMainViewport()->Size;
+  ImGui::SetNextWindowSize(displaySize);
+  ImGui::SetNextWindowPos(ImVec2(0, 0));
+  ImGui::Begin(_label.c_str(), nullptr, flags);
 }
+
+void NodeEditorWindow::begin() {
+  Parent::begin();
+  if (ImGui::BeginMainMenuBar()) {
+
+    // File Menu
+
+    if (ImGui::BeginMenu("File")) {
+      if (ImGui::MenuItem("Exit")) {
+        exitEvent();
+      }
+
+      ImGui::EndMenu();
+    }
+
+    // Render Registration-based windows
+    for (auto [item, infoVec] : _menus) {
+      if (ImGui::BeginMenu(item.c_str())) {
+        for (auto info : infoVec) {
+          if (ImGui::MenuItem(info->name.c_str())) {
+            info->create();
+          }
+        }
+        ImGui::EndMenu();
+      }
+    }
+  }
+  ImGui::EndMainMenuBar();
+}
+
+} // namespace fr::Imgui
--- src/NodeWindow.cpp
+++ src/NodeWindow.cpp
@@ -18,105 +18,100 @@
 
 namespace fr::Imgui {
 
-  bool NodeWindow::_defaultEditable = false;
-  bool NodeWindow::_defaultDisplayEditabilityCheckbox = true;
+bool NodeWindow::_defaultEditable = false;
+bool NodeWindow::_defaultDisplayEditabilityCheckbox = true;
 
-  NodeWindow::NodeWindow(const std::string& label) :
-    Parent(label),
-    _editable(_defaultEditable),
-    _displayEditable(_defaultDisplayEditabilityCheckbox),
-    _displayDebugButton(false),
-    _initted(false) {
-    memset(_idText, '\0', idTextLen);
-    ImU32 white = IM_COL32(255,255,255,255);
-    ImU32 red = IM_COL32(255,0,0,255);
-    _upAnchor = std::make_shared<NodeAnchor>("##UpAnchor", ImVec2(0,0), 5.0, white, red, AnchorType::Up);
-    _downAnchor = std::make_shared<NodeAnchor>("##DownAnchor", ImVec2(0,0), 5.0, white, red, AnchorType::Down);
-    _enableEditingLabel = getUniqueLabel("Enable Editing");
-    _nodeIdLabel = getUniqueLabel("##ID:");
-    _debugButtonLabel = getUniqueLabel("DEBUG!");
-  }
-
-  void NodeWindow::addNode(fr::RequirementsManager::Node::PtrType node) {
-    _node = node;
-  }
-
-  fr::RequirementsManager::Node::PtrType NodeWindow::getNode() {
-    return _node;
-  }
-
-  void NodeWindow::setDisplayDebugButton(bool d) {
-    _displayDebugButton = d;
-  }
-
-  void NodeWindow::init() {
-    if (!_node) {
-      _node = std::make_shared<fr::RequirementsManager::Node>();
-    }
-    if (!_node->initted) {
-      _node->init();
-    }
-    this->addWidget(_upAnchor->getLabel(), _upAnchor);
-    this->addWidget(_downAnchor->getLabel(), _downAnchor);
-    
-    // Set up a lambda so the anchor point render better as the
-    // window is being moved.
-    auto sub = this->moved.connect([&](Window::PtrType parent,
-                                       const ImVec2& pos) {
-      _min = pos;
-      // This always places the upListcenter anchor
-      // 25 pixels below the titlebar
-      ImVec2 upListCenter(_currentSize.x / 2.0, 25.0);
-      _upAnchor->setCenter(screenCoordinate(upListCenter));
-      // There seems to be some slop on the bottom, so
-      // _currentSize is more than adequate as an offset from
-      // there (I should check and see if I can make it a positive
-      // number and exceed the bound down into the slop.
-      ImVec2 downListCenter(_currentSize.x / 2.0, _currentSize.y);
-      _downAnchor->setCenter(screenCoordinate(downListCenter));
-    });
-    
-    _subscriptions.push_back(sub);
-    
-    _initted = true;
-  }
-
-  std::string NodeWindow::idString() {
-    std::string ret;
-    if (_node) {
-      ret = _node->idString();
-    }
-    return ret;
-  }
-
-  void NodeWindow::beginning() {
-    if (!_node || !_initted) {
-      init();
-    }
-    if (!strlen(_idText)) {
-      setIdText();
-    }
-    Parent::beginning();
-  }
-  
-  void NodeWindow::begin() {
-    Parent::begin();
-
-    if (_displayDebugButton) {
-      if (ImGui::Button(_debugButtonLabel.c_str())) {
-        std::cout << std::endl << _node->to_json() << std::endl;
-      }
-    }
-    
-    if (_displayEditable) {
-      ImGui::Checkbox(_enableEditingLabel.c_str(), &_editable);
-    }
-    ImGui::Text("Node ID: ");
-    ImGui::SameLine();
-    ImGui::InputText(_nodeIdLabel.c_str(), _idText, idTextLen - 1, ImGuiInputTextFlags_ReadOnly);
-  }
-
-  void NodeWindow::end() {
-    Parent::end();      
-  }
+NodeWindow::NodeWindow(const std::string &label)
+    : Parent(label), _editable(_defaultEditable),
+      _displayEditable(_defaultDisplayEditabilityCheckbox),
+      _displayDebugButton(false), _initted(false) {
+  memset(_idText, '\0', idTextLen);
+  ImU32 white = IM_COL32(255, 255, 255, 255);
+  ImU32 red = IM_COL32(255, 0, 0, 255);
+  _upAnchor = std::make_shared<NodeAnchor>("##UpAnchor", ImVec2(0, 0), 5.0,
+                                           white, red, AnchorType::Up);
+  _downAnchor = std::make_shared<NodeAnchor>("##DownAnchor", ImVec2(0, 0), 5.0,
+                                             white, red, AnchorType::Down);
+  _enableEditingLabel = getUniqueLabel("Enable Editing");
+  _nodeIdLabel = getUniqueLabel("##ID:");
+  _debugButtonLabel = getUniqueLabel("DEBUG!");
 }
+
+void NodeWindow::addNode(fr::RequirementsManager::Node::PtrType node) {
+  _node = node;
+}
+
+fr::RequirementsManager::Node::PtrType NodeWindow::getNode() { return _node; }
+
+void NodeWindow::setDisplayDebugButton(bool d) { _displayDebugButton = d; }
+
+void NodeWindow::init() {
+  if (!_node) {
+    _node = std::make_shared<fr::RequirementsManager::Node>();
+  }
+  if (!_node->initted) {
+    _node->init();
+  }
+  this->addWidget(_upAnchor->getLabel(), _upAnchor);
+  this->addWidget(_downAnchor->getLabel(), _downAnchor);
+
+  // Set up a lambda so the anchor point render better as the
+  // window is being moved.
+  auto sub =
+      this->moved.connect([&](Window::PtrType parent, const ImVec2 &pos) {
+        _min = pos;
+        // This always places the upListcenter anchor
+        // 25 pixels below the titlebar
+        ImVec2 upListCenter(_currentSize.x / 2.0, 25.0);
+        _upAnchor->setCenter(screenCoordinate(upListCenter));
+        // There seems to be some slop on the bottom, so
+        // _currentSize is more than adequate as an offset from
+        // there (I should check and see if I can make it a positive
+        // number and exceed the bound down into the slop.
+        ImVec2 downListCenter(_currentSize.x / 2.0, _currentSize.y);
+        _downAnchor->setCenter(screenCoordinate(downListCenter));
+      });
+
+  _subscriptions.push_back(sub);
+
+  _initted = true;
+}
+
+std::string NodeWindow::idString() {
+  std::string ret;
+  if (_node) {
+    ret = _node->idString();
+  }
+  return ret;
+}
+
+void NodeWindow::beginning() {
+  if (!_node || !_initted) {
+    init();
+  }
+  if (!strlen(_idText)) {
+    setIdText();
+  }
+  Parent::beginning();
+}
+
+void NodeWindow::begin() {
+  Parent::begin();
+
+  if (_displayDebugButton) {
+    if (ImGui::Button(_debugButtonLabel.c_str())) {
+      std::cout << std::endl << _node->to_json() << std::endl;
+    }
+  }
+
+  if (_displayEditable) {
+    ImGui::Checkbox(_enableEditingLabel.c_str(), &_editable);
+  }
+  ImGui::Text("Node ID: ");
+  ImGui::SameLine();
+  ImGui::InputText(_nodeIdLabel.c_str(), _idText, idTextLen - 1,
+                   ImGuiInputTextFlags_ReadOnly);
+}
+
+void NodeWindow::end() { Parent::end(); }
+} // namespace fr::Imgui
